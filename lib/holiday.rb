46c27f84e346022f06459852cf323aa34b34f4c0
Brad Schrag <brad.schrag@gmail.com>
2018-11-09 04:52:28 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |holiday_name, supply_array|
-      if holiday_name.include?("_")
+      if holiday_name.to_s.include?("_")
         holiday_name.sub("_", " ")
       end
       puts "  #{holiday_name.to_s.capitalize}: #{supply_array.join(", ")}"
